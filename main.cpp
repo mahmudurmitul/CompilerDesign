f2f9a1099c86888530e308070a6b6dbd97be409e
mahmudurmitul <mahmudurmitul714@gmail.com>
2025-11-12 22:57:49 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void check_identifier(string s);
+
+int main()
+{
+    ifstream file("sample.txt");
+    if (!file)
+    {
+        cout << "Error: Could not open sample.txt file!" << endl;
+        return 1;
+    }
+
+    string s;
+    cout << "Checking Identifiers from sample.text\n" << endl;
+
+    while (file >> s)
+    {
+        check_identifier(s);
+    }
+
+    file.close();
+    return 0;
+}
---
+++ problem5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+void check_identifier(string s)
+{
+    string keywords[] = {"int", "float", "double", "char", "for","while", "if", "else", "return", "void"};
+
+    for (int k = 0; k < 10; k++)
+    {
+        if (s == keywords[k])
+        {
+            cout << s << "=  Not Identifier (Keyword)" << endl;
+            return;
+        }
+    }
+    int flag = 0;
+
+    if ((s[0] >= 'a' && s[0] <= 'z') ||
+        (s[0] >= 'A' && s[0] <= 'Z') ||
+        s[0] == '_')
+    {
+        flag = 1;
+
+        for (int i = 1; i < s.length(); i++)
+        {
+            if ((s[i] >= 'a' && s[i] <= 'z') ||
+                (s[i] >= 'A' && s[i] <= 'Z') ||
+                s[i] == '_' ||
+                (s[i] >= '0' && s[i] <= '9'))
+            {
+                flag = 1;
+            }
+            else
+            {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if (flag == 1)
+        cout << s << "=  Identifier" << endl;
+    else
+        cout << s << "=  Not Identifier" << endl;
+}
+
